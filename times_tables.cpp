de0fc0c62ff788f12484ea4e9f38c24cd5151e48
g3th <theinkandpen@yahoo.co.uk>
2022-10-25 23:24:15 +0100

    times table function

---
+++ times_tables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+string multiply(int num){
+	int multiplier = 1;
+	string ans;
+	while (multiplier <= 10){
+		ans = to_string(num)+" x "+ to_string(multiplier) + " = " + to_string(num * multiplier);
+		cout << ans << endl;
+		++ multiplier;
+	}
+	return ans;
+}
+
+int main(){
+	int multiplicand;
+	cout << "Enter multiplicand: " << endl;
+	cin >> multiplicand;
+	cout << "Here is your times table: "<< endl;
+	multiply(multiplicand);
+}
